af2a88c16ef9e73498018fc044d1423bd6946212
Danilo Manchon <danilomanchon@gmail.com>
2024-02-19 22:14:37 -0300

    initial commit

---
+++ src/main/java/com/backend/rinhajava/RinhaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.rinhajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinhaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinhaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/rinhajava/RinhaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.rinhajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
